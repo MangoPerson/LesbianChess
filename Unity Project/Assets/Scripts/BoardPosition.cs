da777f4ae6f13556c1b913517c21877e4d528b35
MangoPerson <79617062+MangoPerson@users.noreply.github.com>
2022-01-22 17:57:03 -0500

    Add Highlighting
    Add highlighting of squares that pieces are being moved from, as well as removing unused references to en passant.

--- Unity Project/Assets/Scripts/BoardPosition.cs
+++ Unity Project/Assets/Scripts/BoardPosition.cs
@@ -5,53 +5,30 @@ using UnityEngine;
 public class BoardPosition
 {
     public List<Piece> pieces;
-    public int en_passant;
     public bool wMove;
     GameObject board;
 
     public BoardPosition(List<int> indices, List<int> types)
     {
-        en_passant = -1;
         wMove = true;
         Create(indices, types);
     }
 
     public BoardPosition(List<int> indices, List<int> types, int en_passant, bool wMove)
     {
-        this.en_passant = en_passant;
         this.wMove = wMove;
         Create(indices, types);
     }
 
     void Create(List<int> indices, List<int> types)
     {
-        Color lightCol = GameObject.Find("Central").GetComponent<Central>().lightCol;
-        Color darkCol = GameObject.Find("Central").GetComponent<Central>().darkCol;
-
         GameObject board = new GameObject();
         board.transform.position = new Vector3(0, 0, 1);
-        SpriteRenderer spr_rend = board.AddComponent<SpriteRenderer>();
-
-        Texture2D tex = new Texture2D(8, 8);
-
-        for (int f = 0; f < 8; f++)
-        {
-            for (int r = 0; r < 8; r++)
-            {
-                bool isLight = (f + r) % 2 == 1;
-                Color squareColor = isLight ? lightCol : darkCol;
-                tex.SetPixel(f, r, squareColor);
-            }
-        }
-        tex.filterMode = FilterMode.Point;
-
-        tex.Apply();
-
-        Sprite spr = Sprite.Create(tex, new Rect(0, 0, 8, 8), new Vector2(.5f, .5f));
-        spr_rend.sprite = spr;
-        spr_rend.transform.localScale = 100 * new Vector3(1, 1, 1);
-
+        board.AddComponent<SpriteRenderer>();
         this.board = board;
+
+        Central central = GameObject.Find("Central").GetComponent<Central>();
+        DrawBoard(central.lightCol, central.darkCol);
 
         pieces = new List<Piece>();
         for (int i = 0; i < indices.Count; i++)
@@ -63,6 +40,44 @@ public class BoardPosition
             piece.bpos = this;
             pieces.Add(piece);
         }
+    }
+
+    public void DrawBoard(Color lightCol, Color darkCol, Color highlightCol, List<int> highlighted)
+    {
+        SpriteRenderer spr_rend = this.board.GetComponent<SpriteRenderer>();
+
+        Texture2D tex = new Texture2D(8, 8);
+
+        for (int f = 0; f < 8; f++)
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                bool isLight = (f + r) % 2 == 1;
+                bool isHighlighted = false;
+                if (highlighted != null)
+                {
+                    if (highlighted.Contains(8 * r + f))
+                    {
+                        isHighlighted = true;
+                    }
+                }
+                Color squareColor = isLight ? lightCol : darkCol;
+                squareColor = isHighlighted ? highlightCol : squareColor;
+                tex.SetPixel(f, r, squareColor);
+            }
+        }
+        tex.filterMode = FilterMode.Point;
+
+        tex.Apply();
+
+        Sprite spr = Sprite.Create(tex, new Rect(0, 0, 8, 8), new Vector2(.5f, .5f));
+        spr_rend.sprite = spr;
+        spr_rend.transform.localScale = 100 * new Vector3(1, 1, 1);
+    }
+
+    public void DrawBoard(Color lightCol, Color darkCol)
+    {
+        DrawBoard(lightCol, darkCol, Color.black, new List<int> { });
     }
 
     public void UpdateBoard(Move move)
